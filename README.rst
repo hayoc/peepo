c19b7564190d500191ab82def55220d431ffe7df
hayoc <hayo.ce@gmail.com>
2017-11-08 21:00:10 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+**Personhood through Predictive Processing**
+============================================
+
+For an introduction, see A. Clark (2013): Whatever next? Predictive brains, situated agents, and the future of cognitive science
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Peepo',
+    version='0.1.0',
+    author='Hayo Carrette',
+    author_email='hayo.ce@gmail.com',
+    packages=['peepo'],
+    scripts=[],
+    url='http://conventiont.com',
+    license='LICENSE',
+    description='Personhood through Predictive Processing.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+    ],
+)
