e1cb591262b3c3b2d9c23e6771578d7870030378
hayoc <hayo.ce@gmail.com>
2017-11-14 22:04:38 +0100

    #1 Create an easy way to initialize a PP hierarchy Development

--- peepo/predictive_processing/discrete/hierarchy.py
+++ peepo/predictive_processing/discrete/hierarchy.py
@@ -6,7 +6,6 @@ from peepo.predictive_processing.discrete.region import Region
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
-
 act = np.array([0.9, 0.1])
 actls = np.array([0.3, 0.7])
 
@@ -66,30 +65,56 @@ if dangsafe.error(dangsafehyp, redgreen.getHyp()):
 
 
 graph = {
+    'root': ['A'],
     'A': ['B', 'C'],
     'B': [],
     'C': []
 }
 
-regions = {'A': Region(np.matrix([[0.3, 0.8], [0.7, 0.2]]), name='A'),
+act = {
+    'B': np.array([0.9, 0.1]),
+    'C': np.array([0.3, 0.7])
+}
+
+regions = {'A': Region(np.matrix([[0.3, 0.8], [0.7, 0.2]]), hyp=np.array([0.9, 0.1]), name='A'),
            'B': Region(np.matrix([[0.8, 0.1], [0.2, 0.9]]), name='B'),
            'C': Region(np.matrix([[0.7, 0.4], [0.3, 0.6]]), name='C')}
 
+
 class Hierarchy:
 
-    def __init__(self, graph, regions, par, parhyp):
+    def __init__(self, graph, regions, inputs):
         self.graph = graph
         self.regions = regions
-        self.parent = par
-        self.regions.get(par).setHyp(parhyp)
+        self.inputs = inputs
+        self.rootname = graph.get('root')
 
     def start(self):
-        topregion = regions.get(self.parent)
-        tophyp = topregion.predict()
+        root = self.regions.get(self.rootname)
+        self.predict_flow(root, root.predict(), [])
 
-        children = self.graph.get(self.parent)
-        for child in children:
-            child.setHyp(tophyp)
+    def predict_flow(self, node, hyp, ancestors):
+        ancestors.append(node)
+        node.setHyp(hyp)
+        name = node.__getattribute__('name')
+
+        children = self.graph.get(name)
+
+        if not children:
+            input = self.inputs.get(name)
+            if node.error(hyp, input):
+                self.error_flow(node, input, ancestors)
+        else:
+            for child in children:
+                childnode = regions.get(child)
+                self.predict_flow(childnode, childnode.predict(), ancestors)
+
+    def error_flow(self, node, input, ancestors):
+        node.update(input)
+        if ancestors:
+            self.error_flow(ancestors.pop(), node.getHyp(), ancestors)
+
+
 
 
 
