68cbdc4d03c6a9619a9e3e91bf8de668cbb943be
hayoc <hayo.ce@gmail.com>
2018-01-09 23:31:31 +0100

    #15 Create a purely reflexive creature     * #27Adapt node to allow motor command execution     * #25Connect PP algorithm with bot inputs

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.1.0',
     author='Hayo Carrette',
     author_email='hayo.ce@gmail.com',
-    packages=['peepo'],
+    packages=['peepo', 'peepo.bot', 'peepo.predictive_processing.discrete'],
     scripts=[],
     url='http://conventiont.com',
     license='LICENSE',
     description='Personhood through Predictive Processing.',
     long_description=open('README.rst').read(),
     install_requires=[
-        'scipy', 'numpy',],
+        'scipy', 'numpy'],
 )
