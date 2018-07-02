c03e6e5a9fdb15aa24b57e41a5904afad2e9aca9
hayo <hayo.ce@gmail.com>
2018-07-02 07:35:08 +0000

    # 33 Peepo's world, join sensoryinputs

--- peepo/playground/cointoss.py
+++ peepo/playground/cointoss.py
@@ -4,10 +4,28 @@ import random
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.models import BayesianModel
 
-from peepo.playground.sensory_input_coin import SensoryInputCoin
 from peepo.predictive_processing.v3.generative_model import GenerativeModel
 
 import matplotlib.pyplot as plt
+
+from peepo.predictive_processing.v3.sensory_input import SensoryInput
+
+
+class SensoryInputCoin(SensoryInput):
+
+    def __init__(self, coin_set):
+        super().__init__()
+        self.coin_set = coin_set
+        self.index = 0
+
+    def action(self, node, prediction_error, prediction):
+        pass
+
+    def value(self, name):
+        # 0 = heads, 1 = tails
+        val = np.array([1, 0]) if self.coin_set[self.index] == 0 else np.array([0, 1])
+        self.index += 1
+        return val
 
 
 def paired_set(size):
