527b2ceb10390fa6c623f999aac11964d4f7d27d
hayoc <hayo.ce@gmail.com>
2018-10-10 17:53:16 +0200

    #63 Model Update

--- peepo/predictive_processing/v3/generative_model.py
+++ peepo/predictive_processing/v3/generative_model.py
@@ -117,12 +117,12 @@ class GenerativeModel:
         :type prediction_error: np.array
         :type prediction: np.array
         """
-        #        self.hypothesis_update(node, prediction_error, prediction)
+        self.hypothesis_update(node, prediction_error, prediction)
         # TODO: make the choice more sophisticated, with precision, surprise, yada yada yada
-        if precision < 0.5:
-            self.model_update(node, prediction_error, prediction)
-        else:
-            self.hypothesis_update(node, prediction_error, prediction)
+        # if precision < 0.5:
+        #     self.model_update(node, prediction_error, prediction)
+        # else:
+        #     self.hypothesis_update(node, prediction_error, prediction)
 
     def hypothesis_update(self, node, prediction_error, prediction):
         """
