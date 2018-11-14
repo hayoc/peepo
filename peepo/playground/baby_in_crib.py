e74b074c8131f508c57c439604844eb57bb476c1
hayoc <hayo.ce@gmail.com>
2018-11-14 20:51:38 +0100

    Baby in crib experiment

---
+++ peepo/playground/baby_in_crib.py
@@ -0,0 +1,17 @@
+"""
+Baby in crib experiment.
+
+See: Conjugate Reinforcement of Infant Exploratory Behavior - http://dx.doi.org/10.1016/0022-0965(69)90025-3
+
+A baby is placed in a crib with a mobile. One of the baby's limbs (e.g. left foot) is attached to the crib with
+a ribbon. Due to motor babbling the baby comes to know the causal effect of moving its left foot which results in
+the mobile moving. Once the ribbon is cut and tied to another limb (e.g. right foot), the baby will display a short
+period of time of frantic movement of the left food (i.e. the first limb connected) but after a while realize that
+the causal structure of the world has shifted and that the mobile is now connected to its right foot.
+
+In terms of predictive processing we expect first little prediction error as the baby is merely exploring. Once it
+expects the mobile to be attached to its left foot prediction error will increase as it tries to intentionally move
+the mobile, until it has sufficiently learned the causal model and can consistently move the mobile. Once the ribbon
+is moved to the other limb - a large increase in prediction error should be witnessed, until this again lowers as the
+baby learns the new causal model.
+"""
