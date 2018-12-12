233d69af5e65efdbbee95c32ec6003e8a5b31cdf
Bufo <oooj4c@otenet.gr>
2018-12-12 20:37:24 +0100

    Further work on Utilities class

--- peepo/utilities/utilities.py
+++ peepo/utilities/utilities.py
@@ -5,29 +5,72 @@ import networkx as nx
 import numpy as np
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.models import BayesianModel
+import re
 from peepo.visualize.graph import draw_network
-
+import matplotlib.pyplot as plt
 
 class Utilities(object):
     def __init__(self):
         pass
 
-    def translation(dictionary, astring):
+
+    def save_json(file, astring):
+        """
+        This helping function is only needed to have the json file be formatted in a user friendly way
+        as the "dump" method does not provide a lot of possibilities to get it "pretty"
+
+        :param file :the ull path of the json file
+        :param astring: the name of the string containing the whole information
+        :return: void
+        :type file: string
+        :type astring : string
+        :rtype : void
+        """
+        text_file = open(file, "w")
+        astring = re.sub('\n', '', astring)
+        astring = re.sub('\"Identification', '\n\"Identification', astring)
+        astring = re.sub('\"Date', '\n\"Date', astring)
+        astring = re.sub('\"Description', '\n\"Description', astring)
+        astring = re.sub('\"Train_from', '\n\"Train_from', astring)
+        astring = re.sub('\"Frozen', '\n\"Frozen', astring)
+        astring = re.sub('\"Nodes', '\n\n\"Nodes', astring)
+        astring = re.sub('\"RONS', '\n\t\t\"RONS', astring)
+        astring = re.sub('\"BENS', '\n\t\t\t\"BENS', astring)
+        astring = re.sub('\"MEMS', '\n\t\t\t\"MEMS', astring)
+        astring = re.sub('\"LANS', '\n\t\t\"LANS', astring)
+        astring = re.sub('\"LENS', '\n\t\t\"LENS', astring)
+        astring = re.sub('\"MOTOR', '\n\t\t\t\"MOTOR', astring)
+        astring = re.sub('\"WORLD', '\n\t\t\t\"WORLD', astring)
+        astring = re.sub('\"Edges', '\n\n\"Edges', astring)
+        astring = re.sub('\"CPDs', '\n\n\"CPDs', astring)
+        astring = re.sub('{', '\n\t\t{', astring)
+        text_file.write(astring)
+        text_file.write('\n')
+        text_file.close()
+
+    def translation(dictionary, astring, from_man_to_machine):
         """
         Given an array of tuples (a,b) in dictionary, returns the second element of the tuple where astring was found
         Is used to not loose the users node names as peepo generates standardized names for the corresponding node
 
         :param dictionary:an array of tuples -> is created in the method : get_network(file)
         :param astring: the name of the node passsed by the user
+        :param from_man_to_machine: an integer -> 0 when we want the translation for the user give name to the standardized name, 1 the other way around
         :return: the corresponding standardized node name
         :type dictionary: np.array
         :type astring : string
         :rtype : string
         """
+        source = 0
+        target = 1
+        if from_man_to_machine == 1:
+            source = 1
+            target = 0
+
         for index, item in enumerate(dictionary):
-            if item[0] == astring :
+            if item[source] == astring :
                 break
-        return  item[1]
+        return  item[target]
 
     def check_json_path(directory):
         """
@@ -93,44 +136,48 @@ class Utilities(object):
 
         :param file: : filename without path or extension
         :pgmp_object : the pgmpy object which will be completed
-        :return: a dictionary as an array of tuples
+        :return: a dictionary as an array of tuples and the header of the json file
 
         :type file : string
         :type pgmp_object : pgmpy class object
-        :rtype : array of tuples
+        :rtype : array of tuples, dictionary
         """
-        network, node_dictionary =  Utilities.get_network(file)
+        network, node_dictionary, header =  Utilities.get_network(file)
         """Iterating through the rootnodes (get's only a 1-dimensional arry"""
         nw_nodes = network.nodes(data = True)
 
         for i, node in enumerate(nw_nodes):
             parent_nodes = network.predecessors(node[0])
             child_nodes = network.successors(node[0])
-            """Add edges"""
+            """Add nodes and edges in one loop"""
             if len(child_nodes) != 0:
                 for child in child_nodes:
                     pgmpy_object.add_edge(node[0], child)
+            """ add cpd's"""
             cpd = node[1]['cpd']
+            ''' find the cardinality of the node and it's parents'''
+            cardinality_node = node[1]['cardinality']
             cardinality_parents = []
             for i,nod in enumerate(parent_nodes):
-                cpd_nod = network.node[nod]
-                cardinality_parents.append(len(cpd_nod['cpd']))
-            cardinality_node  = len(cpd)
+                attribute = network.node[nod]
+                cardinality_parents.append(attribute['cardinality'])
+            ''' Depending on the place in the BN and/or the number of parents  the CPD have another call'''
             if len(cardinality_parents) == 0:
                 pgmpy_object.add_cpds(TabularCPD(variable=node[0], variable_card= cardinality_node, values=[cpd]))
                 continue
-            if len(parent_nodes) == 1:
-                cpd_par = network.node[parent_nodes[0]]
-                cardinality_parents = [len(cpd_par['cpd'])]
+            if len(parent_nodes) == 1:#this is necessary so the TabularCPD function accepts only one parent (evidence_card must be reshaped)
+                attribute = network.node[parent_nodes[0]]
+                cardinality_parents = [attribute['cardinality']]
             table = TabularCPD(variable=node[0], variable_card= cardinality_node, values=cpd, \
                               evidence=parent_nodes,\
                               evidence_card=cardinality_parents)
             pgmpy_object.add_cpds(table)
 
-        # pgmpy_object.check_model()
-        # draw_network(pgmpy_object)
-
-        return node_dictionary
+        '''------TO DELETE-------------'''
+        pgmpy_object.check_model()
+        draw_network(pgmpy_object)
+        '''-----------------------------'''
+        return node_dictionary, header
 
 
     def get_network(file):
@@ -140,24 +187,27 @@ class Utilities(object):
         - Creates a dictionary for the nodes in the form of an array of tuples : [(names defines by user, standard name)]
 
         :param file: : filename without path or extension
-        :return: a networkx class object and dictionary as an array of tuples
+        :return: a networkx class object, dictionary as an array of tuples and the header of the json file
 
         :type file : string
-        :rtype : networkx class object, array of tuples
+        :rtype : networkx class object, array of tuples, dictionary
         """
         with open(Utilities.get_json_path(file)) as f:
             data = json.load(f)
+        header = data['header']
         G = nx.DiGraph()
 
         '''Feeding G with the nodes'''
         nodes = []
         node_dictionary = []
+        cardinality = {}
         for key in data['Nodes'].keys():
             for  secondkey in data['Nodes'][key].keys():
                 for c, n  in enumerate(data['Nodes'][key][secondkey]):
                     node = secondkey + "_" + str(c)
                     nodes.append(node)
-                    node_dictionary.append((n,node))
+                    node_dictionary.append((n[0],node))
+                    cardinality.update({node:n[1]})#this containe the cardinality of each node with the node name as dictionary entry
         np.ravel(nodes)
         G.add_nodes_from(nodes)
 
@@ -166,23 +216,31 @@ class Utilities(object):
         for j, pair in enumerate(data['Edges']):
             for parent in pair.keys():
                 for child in data['Edges'][j][parent]:
-                    parent_ = Utilities.translation(node_dictionary,parent)
-                    child_  = Utilities.translation(node_dictionary,child)
+                    parent_ = Utilities.translation(node_dictionary,parent, 0)
+                    child_  = Utilities.translation(node_dictionary,child, 0)
                     edges.append((parent_,child_))
         np.ravel(edges)
         G.add_edges_from(edges)
 
-        '''Feeding G with the CPD's as nodes attributes'''
+        '''Feeding G with the cardinality and CPD's as nodes attributes'''
         for j, node in enumerate(data['CPDs']):
-            for parent in node.keys():
-                node_ = Utilities.translation(node_dictionary, parent)
-                cpds = {'cpd':data['CPDs'][j][parent]}
-                G.add_node(node_ ,cpds)
+            for parent, cpd in node.items():
+                node_ = Utilities.translation(node_dictionary, parent, 0)
+                addendum = {'cardinality': cardinality[node_], 'cpd': cpd}
+                G.add_node(node_ ,addendum)
 
-        return G, node_dictionary
+        '''TO REMOVE LATER'''
+        plt.figure(figsize=(10, 5))
+        pos = nx.circular_layout(G, scale=2)
+        node_labels = nx.get_node_attributes(G, 'cpd')
+        nx.draw(G, pos, node_size=1200, node_color='lightblue',
+                linewidths=0.25,  font_size=10, font_weight='bold', with_labels=True)
+        plt.show()
+        return G, node_dictionary, header
 
 
-    def create_json_file(case_name, identification_ = None, description_ = None, frozen_ = None, nodes_ = None, edges_ = None, cpds_ = None, train_from_ = None):
+    def create_json_file(Case_name, identification_ = None, description_ = None, frozen_ = None, nodes_ = None, edges_ = None, cpds_ = None, train_from_ = None):
+
         """
         A helping method if the user prefers to create the BN within the code
 
@@ -200,26 +258,27 @@ class Utilities(object):
         .
         :rtype : void
         """
-        Case_name = Utilities.get_json_path(case_name)  # creates the right path in which case_name will be saved
+        case_name = Utilities.get_json_path(Case_name)  # creates the right path in which case_name will be saved
+        #json_tab_file_write = JSONTabIndentFileWriter( Case_name,5a)
         data = Utilities.get_empty_canvas()
 
         '''       - the 3 next items are for tracking purpose only, not fundamentally necessary'''
-        data['Identificaton'] = case_name
-        data['Date'] = datetime.datetime.now().strftime("%c")
-        data['Description'] = 'Blablabla'
+        data["header"]['Identification'] = case_name
+        data["header"]['Date'] = datetime.datetime.now().strftime("%c")
+        data["header"]['Description'] = 'Blablabla'
         '''       - the next item gives a file containing possible training data (OPTIONAL)'''
-        data['Train_from'] = 'None'
+        data["header"]['Train_from'] = 'None'
 
         '''      Frozen tells whether or not the model can be considered as final i.e. is there still "training" needed'''
-        data['Frozen'] = False
+        data["header"]['Frozen'] = False
 
         '''       - the 5 next lines tells how much nodes  and their names the model will start with
                     the names can be any valid python string'''
-        bens = ['pooping', 'peeing', 'constipated']
-        mems = ['havenotoiletpaper']
-        lans = ['diarhea', 'happypoop']
-        motors = ['asshole1', 'asshole2']
-        world = ['toilet1', 'toilet2', 'garden1', 'garden2','doctor']
+        bens = [['pooping',2], ['peeing',2], ['constipated',2]]
+        mems = [['havenotoiletpaper',2]]
+        lans = [['diarhea',2], ['happypoop',2]]
+        motors = [['asshole1',2], ['asshole2',2]]
+        world = [['toilet1',2], ['toilet2',2], ['garden1',2], ['garden2',2],['doctor',2]]
 
         '''     - the next items describe the edges as a dictionary
                  -> the dictionary entry is always one of the rootnodes, the array following can only contain LANs or LENs'''
@@ -257,16 +316,19 @@ class Utilities(object):
         '''       - feeding the data'''
         data["Nodes"]['RONS']['BENS']  = bens
         data["Nodes"]['RONS']['MEMS']  = mems
-        data["Nodes"]['LANS']['LANS']  = lans
+        data["Nodes"]['LANS']['LANS']= lans
         data["Nodes"]['LENS']['MOTOR'] = motors
         data["Nodes"]['LENS']['WORLD'] = world
         data["Edges"] = edges
         data["CPDs"] = cpds
 
-
-        ''' dumping to Case_name file in jason format'''
-        with open(Case_name, 'w') as f:
-            json.dump(data, f, indent=3)
+        # json.dump(data,json_tab_file_write, indent=5, separators=('}', ': '), sort_keys=False)
+        # # close file writer
+        # json_tab_file_write.close()
+        ''' dumping to CASENAME file in jason format'''
+        # with open(case_name, 'w') as f:
+        #     json.dump(data, f, separators = (",",":"))
+        Utilities.save_json(case_name,json.dumps(data))
 
         print("Json file for  - ", case_name, "  - created")
 
@@ -283,9 +345,31 @@ class Utilities(object):
         case_name = "Template"
         case_name = Utilities.get_json_path(case_name)  # creates the right path in which case_name will be saved
         data = Utilities.get_empty_canvas()
+        data['header']['Identification'] = case_name
+        '''Filling some dummies to facilitate the user'''
+        a_node = ['*',0]
+        an_edge = {'*':['&','&','&']}
+        a_cpd = {'*':[[0,0,0],[0,0,0]]}
+        nodes = []
+        edges = []
+        cpds = []
+        for i in range(0,3):
+            nodes.append(a_node)
+            edges.append(an_edge)
+            cpds.append(a_cpd)
+
+        data['Nodes']['RONS']['BENS'] = nodes
+        data['Nodes']['RONS']['MEMS']= nodes
+        data['Nodes']['LANS']['LANS'] = nodes
+        data['Nodes']['LENS']['MOTOR'] = nodes
+        data['Nodes']['LENS']['WORLD'] =nodes
+        data['Edges'] = edges
+        data['CPDs'] = cpds
+
         ''' dumping to CASENAME file in jason format'''
-        with open(case_name, 'w') as f:
-            json.dump(data, f, indent=3)
+        # with open(case_name, 'w') as f:
+        #     json.dump(data, f, separators = (",",":"))
+        Utilities.save_json(case_name,json.dumps(data))
         print("Empty template created")
 
     def get_empty_canvas():
@@ -298,8 +382,8 @@ class Utilities(object):
          :rtype : dictionary
          """
 
-        data = {'Identificaton': '', 'Date': '', 'Description': '', 'Frozen': '', 'Nodes': [], 'Edges': [], 'CPDs': [],
-                'Train_from': ''}
+        data = {'header': {'Identification': '', 'Date': '', 'Description': '', 'Frozen': '', 'Train_from': ''},
+                'Nodes': {}, 'Edges': [], 'CPDs': []}
 
         '''       - the 5 next lines tells how much nodes  and their names the model will start with
                     the names can be any valid python string'''
@@ -320,9 +404,9 @@ class Utilities(object):
         cpds = []
 
         '''       - feeding the data'''
-        data['Nodes']= {'RONS': {'BENS': bens, 'MEMS': mems}, 'LANS':{'LANS': lans}, 'LENS': {'MOTOR': motors, 'WORLD': world}}
-        data['Edges'].append(edges)
-        data['CPDs'].append(cpds)
+        data['Nodes']= {'RONS': {'BENS': bens, 'MEMS': mems}, 'LANS':{'LANS':lans}, 'LENS': {'MOTOR': motors, 'WORLD': world}}
+        data['Edges']= edges
+        data['CPDs'] = cpds
         return data
 
 def main():
@@ -339,6 +423,8 @@ def main():
         else:
             Utilities.create_json_file(str(var))
     network = BayesianModel()
-    dic = Utilities.get_pgmpy_network(var, network)
+    dic, header = Utilities.get_pgmpy_network(var, network)
+    print("Dictionary  ", dic)
+    print("header ",  header)
 if __name__ == "__main__":
     main()
