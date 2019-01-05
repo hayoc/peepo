70230d296964f8939d75f3a2963f23c85655d028
Bufo <oooj4c@otenet.gr>
2019-01-05 10:12:07 +0200

    added a small utility for cpd creating and altering in the framework of GA

--- peepo/tests/parametrized_child_cpd_test.py
+++ peepo/tests/parametrized_child_cpd_test.py
@@ -6,43 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-"""
-Coding comments:
-- Try to avoid creating classes when you can. Object-oriented programming (= focusing on the creation of objects, each
-    with their own states and properties) is a nice way of programming when needed, but only when needed. In this case
-    for example we are writing utility functions, there is no need to create any objects. We want the user of these
-    functions just to call the function with certain parameters X and get a result Y back.
-- So try to abstract away as much as possible in your functions. You still had to calculate the omega and then loop
-    through num_check as a user. You could put all that logic in the function itself, so that the user can call one
-    single function very simply, e.g. pdf = mutated_cpd(my_card_child, my_card_parents, my_num_check)
-
--   self.phase_shift = np.dtype('d')
-    self.phase_shift = omega[0]
-    -> This doesn't actually do anything. You're assigning the variable to one value (i.e. the dtype) and then you're
-    assigning it a new value
-- I think what you were trying to do there is just set the type of the array. Like in this line:
-    self.omega = np.array(omega,dtype = np.dtype('d'))
-  but this is also not necessary I think. 
-- This is only for the plotting part but I changed one line from
-    ax = fig.gca(projection='3d')
-  to 
-    ax = Axes3D(fig)
-  The reason I did that is because I wanted to remove the import statements you didn't use (you should do that as well,
-  you can do it automatically by pressing Ctrl + Alt + O (or go to your top bar to Code > Optimize Imports). It's not
-  just a matter of style but also you're importing libraries for no reason which might slow things down.
-
-
-
-General Style Comments:
-- Try to Reformat your code once in a while, you often have style issues which PyCharm doesn't like (it will display it
-    with a yellow squiggly line below it (e.g. this is a green squiggly one: fhdnkfjskdf). You can reformat your code 
-    automatically by pressing Ctrl + Alt + L (or go to your top bar to Code > Reformat Code)
-- Comment blocks should be done with 3 double quotes, you always use 3 single quotes (i.e. " vs '). These comment
-    blocks should be for descriptions of methods and classes. If you want a single line comment somewhere inside a
-    method between lines of code, use # (which is much easier than typing 3 single quotes) (See some examples below)
-
-"""
-
 
 def ga_child_cpd(card_child, card_parents, omega):
     """
@@ -50,8 +13,8 @@ def ga_child_cpd(card_child, card_parents, omega):
 
     :param card_child: integer = childs' cardinality
     :param card_parents: array containing the cardinality of the childs' parents
-    :param omega: an array of dim card_child containing the generating cpd's parameters
-    :return: a matrix of shape (card_child, numpy.prod(card_parents) conting the conditional probability distribution
+    :param omega: an array of dim card_child containing the cpd's  generating parameters
+    :return: a matrix of shape (card_child, numpy.prod(card_parents) containing the conditional probability distribution
 
     example :
 
@@ -102,10 +65,9 @@ if __name__ == '__main__':
     my_card_child = 3
     max_omega = 2 * math.pi * np.prod(my_card_parents)
     my_omega = np.random.rand(my_card_child) * max_omega
-    my_omega = [2.0,0.1,0.9]
     my_pdf = ga_child_cpd(my_card_child, my_card_parents, my_omega)
-    np.set_printoptions(formatter={'float': lambda my_pdf: "{0:0.2f}".format(my_pdf)})
-    print(my_pdf)
+    # np.set_printoptions(formatter={'float': lambda my_pdf: "{0:0.2f}".format(my_pdf)})
+    # print(my_pdf)
 
     # ----------- PLOTTING ------------------
     fig = plt.figure()
